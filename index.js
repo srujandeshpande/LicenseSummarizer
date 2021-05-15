471e7f23e932e68f785ec787ab429423ad138344
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-05-15 19:21:17 +0530

    Display image properly

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 async function makeImg(licenses, res) {
     const width = 1200
-    const height = 630
+    const height = 650
 
     const canvas = createCanvas(width, height)
     const context = canvas.getContext('2d')
@@ -18,17 +18,45 @@ async function makeImg(licenses, res) {
     context.font = 'bold 30pt Menlo'
     context.textAlign = 'left'
     context.textBaseline = 'top'
-    context.fillStyle = '#3574d4'
 
-    // const textWidth = context.measureText(text).width
-    // context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120)
-    context.fillStyle = '#fff'
-    var w = 100;
+    var w = 180;
+
+    var total = 0;
+    var count = 0;
+    var l = [];
 
     for (const key in licenses) {
-        console.log(`${key}: ${licenses[key]}`);
-        context.fillText(`${key}: ${licenses[key]}`, 60, w);
-        w += 120
+        l.push([licenses[key], key]);
+    }
+
+    function compare(a, b) {
+        if (parseInt(a[0]) > parseInt(b[0])) {
+            return -1;
+        }
+        else {
+            return 1;
+        }
+    }
+    l.sort(compare);
+    count = l.length;
+    for (var i = 0; i < l.length; i++) {
+        total += l[i][0];
+    }
+
+    var colors = ['#581845', '#900C3F', '#C70039', '#FF5733', '#FFC300']
+    var cstart = 60;
+    for (var i = 0; i < l.length; i++) {
+        context.fillStyle = '#fff'
+        context.fillText(`${l[i][1]} - ${l[i][0]}`, 150, w);
+        context.fillStyle = colors[i]
+        context.fillRect(60, w - 10, 60, 60);
+        // Total length = 1080
+        // Start at 60
+        var len = (1080 * l[i][0]) / total;
+        // gap from left, top gap, total length, width of line
+        context.fillRect(cstart, 60, len, 60);
+        cstart += len;
+        w += 90
     }
 
     const buffer = canvas.toBuffer('image/png')
@@ -59,13 +87,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/testing", (req, res) => {
-    var licenses = { 'MIT License': 5, 'Apache License 2.0': 7, Other: 17 }
+    var licenses = { 'MIT License': 5, 'MIT3 License': 15, 'MIT2 License': 2, 'Apache License 2.0': 7, Other: 17 }
     makeImg(licenses, res);
+    res.send("Testing.");
 });
 
 app.get("/:username", (req, res) => {
     // 2 Hours cache browserside
-    // res.set('Cache-control', 'public, max-age=7200');
+    res.set('Cache-control', 'public, max-age=7200');
     let username = req.params.username;
     fetch(`https://api.github.com/users/${username}/repos`)
         .then(response => response.json())
