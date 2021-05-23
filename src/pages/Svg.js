0e3f2567e5265865120e2e9bd8f7c7d68cf2b71c
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2021-05-23 13:13:00 +0530

    Add labels to SVG and fix typo

--- src/pages/Svg.js
+++ src/pages/Svg.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 function Svg({ licenses }) {
-    const cardHeight = 195;
+    const textSpace = 50;
+    const cardHeight = 90 + textSpace * Object.keys(licenses).length;
     const cardWidth = 495;
 
     const barheight = 10;
-    const barY = 30;
+    const barY = 40;
     const barXStart = 50;
     const barWidth = cardWidth - 2 * barXStart;
     const barColors = ["#581845", "#900C3F", "#C70039", "#FF5733", "#FFC300"];
+
+    const textStartX = barXStart;
+    const textStartY = barY + 40;
 
     const licenseList = [];
     var total = 0;
@@ -18,8 +22,9 @@ function Svg({ licenses }) {
             name: key,
             count: parseInt(licenses[key]),
             percentage: 0,
-            startX: 0,
+            barStartX: 0,
             width: 0,
+            textStartY: 0,
         };
         total += licenseDetails.count;
         licenseList.push(licenseDetails);
@@ -35,29 +40,52 @@ function Svg({ licenses }) {
 
     licenseList.sort(compare);
     var prevXLoc = barXStart;
+    var prevTextStartY = textStartY;
     for (const licenseIndex in licenseList) {
         const perc = licenseList[licenseIndex].count / total;
         const width = parseFloat((barWidth * perc).toFixed(2));
 
         licenseList[licenseIndex].percentage = parseFloat(perc.toFixed(2));
-        licenseList[licenseIndex].startX = prevXLoc;
+        licenseList[licenseIndex].barStartX = prevXLoc;
         licenseList[licenseIndex].width = width;
+        licenseList[licenseIndex].textStartY = prevTextStartY;
 
+        prevTextStartY += textSpace;
         prevXLoc += width;
     }
 
-    function createMask(liscenceItem, key) {
+    function createMask(licenseItem, key) {
         return (
             <rect
                 key={key}
                 xmlns="http://www.w3.org/2000/svg"
                 mask="url(#rect-mask)"
-                x={liscenceItem.startX}
+                x={licenseItem.barStartX}
                 y={barY}
-                width={liscenceItem.width}
+                width={licenseItem.width}
                 height={barheight}
                 fill={barColors[key]}
             />
+        );
+    }
+
+    function createLabel(licenseItem, key) {
+        return (
+            <g
+                transform={
+                    "translate(" +
+                    textStartX +
+                    ", " +
+                    licenseItem.textStartY +
+                    ")"
+                }
+                key={key}
+            >
+                <circle cx="5" cy="6" r="5" fill={barColors[key]} />
+                <text x="15" y="10" fill="#fff">
+                    {licenseItem.name + " - " + licenseItem.count}
+                </text>
+            </g>
         );
     }
 
@@ -81,6 +109,7 @@ function Svg({ licenses }) {
                         />
                     </mask>
                     {licenseList.map(createMask)}
+                    {licenseList.map(createLabel)}
                 </g>
             </svg>
         </>
